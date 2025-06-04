733f30cb6dede2dc07e99ffa67ee0c27a2e869fa
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-04 06:43:06 -0700

    Created the WinUI project
    Created the project that will become a WinUI version of the app.

---
+++ WinUIApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIApp
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	public sealed partial class App : Application
+	{
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched.
+		/// </summary>
+		/// <param name="args">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs args) { }
+	}
+}
