19ef2d244dd3367ba571858c8583d4fce38cf8c6
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-07 23:56:21 -0700

    Added JSON converters
    Added the JSON converters used with models.

---
+++ WinUIApp/Converters/TimeSpanConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace WinUIApp.Converters
+{
+	internal sealed class TimeSpanConverter : JsonConverter<TimeSpan>
+	{
+		public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			return TimeSpan.TryParse(reader.GetString(), out TimeSpan result) ? result : TimeSpan.Zero;
+		}
+
+		public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+		{
+			writer.WriteStringValue(value.ToString());
+		}
+	}
+}
