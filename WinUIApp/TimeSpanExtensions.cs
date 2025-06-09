a35ca775f9a8e56cfecbdcf9126e94a2a2fbf3fd
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-08 22:57:22 -0700

    Create TimeSpanExtensions.cs
    Created an extension method for clamping a TimeSpan value.

---
+++ WinUIApp/TimeSpanExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WinUIApp
+{
+	internal static class TimeSpanExtensions
+	{
+		public static TimeSpan Clamp(this TimeSpan value, TimeSpan min, TimeSpan max)
+		{
+			return value < min ? min : value > max ? max : value;
+		}
+	}
+}
