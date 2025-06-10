571931843503964042a468d98e752276352a7564
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-10 03:19:47 -0700

    Create NotEmptyAttribute.cs
    Created an attribute for validating that a value is not empty.

---
+++ WinUIApp/Attributes/NotEmptyAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace WinUIApp.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	internal sealed class NotEmptyAttribute : ValidationAttribute
+	{
+		public override bool IsValid(object? value)
+		{
+			return value switch
+			{
+				string str => !string.IsNullOrWhiteSpace(str),
+				ICollection values => values.Count > 0,
+				IEnumerable objects => objects.OfType<object>().Any(),
+				_ => base.IsValid(value),
+			};
+		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			return string.Format("The field {0} was empty.", name);
+		}
+	}
+}
