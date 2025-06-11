8d193e04c7cea87cf4127c34660827ecc69eb3c0
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-10 17:29:52 -0700

    Create EnumBinding.cs
    Created a way of binding the values of an Enum to a XAML file.

---
+++ WinUIApp/Markup/EnumBinding.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Markup;
+
+namespace WinUIApp.Markup
+{
+	internal sealed partial class EnumBinding : MarkupExtension
+	{
+		[AllowNull]
+		public Type EnumType { get; set; }
+
+		protected override object ProvideValue()
+		{
+			return Enum.GetValues(EnumType).OfType<Enum>();
+		}
+	}
+}
--- WinUIApp/Options/SavableJsonOptions.cs
+++ WinUIApp/Options/SavableJsonOptions.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace WinUIApp.Options
+﻿namespace WinUIApp.Options
 {
 	internal sealed class SavableJsonOptions<TOptions> : ISavableJsonOptions<TOptions>
 		where TOptions : class
