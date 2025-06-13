e72032639b896386fcce07d132b5fffff46b0162
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-12 21:28:17 -0700

    Create ValidationExtensions.cs
    Created the extensions used to validate a control.

--- WinUIApp/Models/ValidationModelBase.cs
+++ WinUIApp/Models/ValidationModelBase.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace WinUIApp.Models
---
+++ WinUIApp/Validation/ValidationExtensions.cs
@@ -0,0 +1,52 @@
+﻿namespace WinUIApp.Validation
+{
+	internal static class ValidationExtensions
+	{
+		private static void CheckForErrors<TModel>(Control control, string bindingPath, string? propertyName)
+			where TModel : notnull, INotifyDataErrorInfo
+		{
+			ArgumentNullException.ThrowIfNull(control, nameof(control));
+			ArgumentException.ThrowIfNullOrWhiteSpace(bindingPath, nameof(bindingPath));
+
+			if (string.IsNullOrWhiteSpace(propertyName) || bindingPath != propertyName)
+			{
+				return;
+			}
+
+			if (control.DataContext is TModel model && control.FindFirstVisualChild<TextBlock>("ErrorContent") is TextBlock textBlock && model.GetErrors(propertyName) is IReadOnlyCollection<string> errors)
+			{
+				_ = VisualStateManager.GoToState(control, errors.Count > 0 ? "Invalid" : "Valid", true);
+
+				textBlock.Text = string.Join(Environment.NewLine, errors);
+			}
+		}
+
+		public static void LoadValidation<TModel>(this Control control)
+			where TModel : notnull, INotifyDataErrorInfo
+		{
+			ArgumentNullException.ThrowIfNull(control, nameof(control));
+
+			if (control is ComboBox comboBox && comboBox.FindFirstVisualChild<Button>("DropDownButton") is Button button)
+			{
+				button.Click += (_, _) => comboBox.IsDropDownOpen = !comboBox.IsDropDownOpen;
+			}
+
+			if (control.DataContext is TModel model)
+			{
+				void Loaded(object? sender, object e)
+				{
+					if (control.GetBindingPath() is string bindingPath)
+					{
+						model.ErrorsChanged += (_, args) => CheckForErrors<TModel>(control, bindingPath, args.PropertyName);
+
+						CheckForErrors<TModel>(control, bindingPath, bindingPath);
+					}
+
+					control.LayoutUpdated -= Loaded;
+				}
+
+				control.LayoutUpdated += Loaded;
+			}
+		}
+	}
+}
