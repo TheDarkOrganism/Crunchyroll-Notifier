30e0eeed95d91ccdbdba1bc950a4facd6ee967dd
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-06-25 03:05:06 -0700

    Add custom error formatting
    Added formatted minimum and maximum range messages to TimeSpanRangeAttribute and implemented FormatErrorMessage to provide detailed validation errors. Introduced helper methods for formatting TimeSpan values in a user-friendly way.

--- WinUIApp/Attributes/TimeSpanRangeAttribute.cs
+++ WinUIApp/Attributes/TimeSpanRangeAttribute.cs
@@ -1,8 +1,14 @@
-﻿namespace WinUIApp.Attributes
+﻿using System.Text;
+
+namespace WinUIApp.Attributes
 {
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
 	internal sealed class TimeSpanRangeAttribute : ValidationAttribute
 	{
+		private readonly string _mininumMessage;
+
+		private readonly string _maxinumMessage;
+
 		public TimeSpan Mininum { get; }
 
 		public TimeSpan Maxinum { get; }
@@ -20,6 +26,9 @@
 
 			Mininum = new(minDays, minHours, minMinutes, minSeconds);
 			Maxinum = new(maxDays, maxHours, maxMinutes, maxSeconds);
+
+			_mininumMessage = Format(Mininum);
+			_maxinumMessage = Format(Maxinum);
 		}
 
 		public TimeSpanRangeAttribute(int minDays, int maxDays, int minHours, int maxHours, int minMinutes, int maxMinutes) : this(minDays, maxDays, minHours, maxHours, minMinutes, maxMinutes, 0, 0) { }
@@ -31,6 +40,53 @@
 		public override bool IsValid(object? value)
 		{
 			return (value is TimeSpan timeSpan || TimeSpan.TryParse(value?.ToString(), out timeSpan)) && timeSpan.Clamp(Mininum, Maxinum) == timeSpan;
+		}
+
+		private static string GetFormatEnd(int count)
+		{
+			return count == 1 ? string.Empty : "s";
+		}
+
+		private static string Format(TimeSpan timeSpan)
+		{
+			StringBuilder stringBuilder = new();
+
+			const string format = "{0} {1}{2} ";
+
+			int days = timeSpan.Days;
+
+			if (days != 0)
+			{
+				stringBuilder = stringBuilder.AppendFormat(format, days, "day", GetFormatEnd(days));
+			}
+
+			int hours = timeSpan.Hours;
+
+			if (hours != 0)
+			{
+				stringBuilder = stringBuilder.AppendFormat(format, hours, "hour", GetFormatEnd(hours));
+			}
+
+			int minutes = timeSpan.Minutes;
+
+			if (minutes != 0)
+			{
+				stringBuilder = stringBuilder.AppendFormat(format, minutes, "minute", GetFormatEnd(minutes));
+			}
+
+			int seconds = timeSpan.Seconds;
+
+			if (seconds != 0)
+			{
+				stringBuilder = stringBuilder.AppendFormat(format, seconds, "second", GetFormatEnd(seconds));
+			}
+
+			return stringBuilder.ToString().Trim();
+		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			return string.Format("The field {0} must be between ({1}) and ({2}).", name, _mininumMessage, _maxinumMessage);
 		}
 	}
 }
