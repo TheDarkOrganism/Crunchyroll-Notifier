a6d8dea27bb29e2ce6051be5843ef479dca7e4a4
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-07-10 12:03:22 -0700

    Add TypeExtensions for validation attributes
    Introduces a static TypeExtensions class with methods to extract validation attributes from public properties, excluding those marked with JsonIgnoreAttribute. Provides overloads for custom key resolution.

---
+++ WinUIApp/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WinUIApp
+{
+	internal static class TypeExtensions
+	{
+		private static bool PropertyFilter(PropertyInfo propertyInfo)
+		{
+			return propertyInfo.GetCustomAttribute<JsonIgnoreAttribute>() is null;
+		}
+
+		private static ValidationAttribute[] GetValidationAttributes(PropertyInfo propertyInfo)
+		{
+			return [.. propertyInfo.GetCustomAttributes<ValidationAttribute>()];
+		}
+
+		public static Dictionary<string, ValidationAttribute[]> GetValidationAttributes([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] this Type type)
+		{
+			return type.GetProperties().Where(PropertyFilter).ToDictionary(static property => property.Name, GetValidationAttributes);
+		}
+
+		public static Dictionary<string, ValidationAttribute[]> GetValidationAttributes([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] this Type type, Func<PropertyInfo, string> keyResolver)
+		{
+			return type.GetProperties().Where(PropertyFilter).ToDictionary(keyResolver, GetValidationAttributes);
+		}
+	}
+}
