1e8f02ea400c606325f3a1f88eaa5411c8b96708
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-07-16 10:29:48 -0700

    Add LanguageAttribute for property validation
    Introduces LanguageAttribute to validate property values against known culture names. Ensures only valid language names or hyphenated combinations are accepted.

---
+++ WinUIApp/Attributes/LanguageAttribute.cs
@@ -0,0 +1,32 @@
+﻿namespace WinUIApp.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	internal sealed class LanguageAttribute : ValidationAttribute
+	{
+		private static readonly StringComparer _stringComparer = StringComparer.OrdinalIgnoreCase;
+
+		private static readonly HashSet<string> _names = CultureInfo.GetCultures(CultureTypes.AllCultures).Skip(1).Select(static culture => culture.DisplayName.Split(' ')[0]).Distinct(_stringComparer).ToHashSet(_stringComparer);
+
+		public override string FormatErrorMessage(string name)
+		{
+			return string.Format("The language {0} is invalid.", name);
+		}
+
+		public override bool IsValid(object? value)
+		{
+			if (value is not string str || string.IsNullOrWhiteSpace(str))
+			{
+				return false;
+			}
+
+			if (_names.Contains(str))
+			{
+				return true;
+			}
+
+			int index = str.LastIndexOf('-');
+
+			return index > -1 && _names.Contains(str[..index]) && _names.Contains(str[(index + 1)..]);
+		}
+	}
+}
