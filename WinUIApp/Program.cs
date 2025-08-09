7c7d2b8673b96b92e0ae0402a50de35d76b77d2b
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-08-09 16:01:50 -0700

    Add custom Program.cs entry point and build check
    Introduces a new Program.cs to handle single-instance logic and application startup. Updates the project file to define DISABLE_XAML_GENERATED_MAIN if Program.cs is present, ensuring the custom entry point is used instead of the default XAML-generated main.

---
+++ WinUIApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Dispatching;
+using WinRT;
+using WinUIApp;
+
+string? processPath = Environment.ProcessPath;
+
+if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(processPath)).Any(process => process.Id != Environment.ProcessId && process.MainModule?.FileName == processPath))
+{
+	Debug.WriteLine("The application was already running.");
+
+	Environment.Exit(0);
+
+	return;
+}
+
+ComWrappersSupport.InitializeComWrappers();
+
+Application.Start(static delegate
+{
+	DispatcherQueueSynchronizationContext context = new(DispatcherQueue.GetForCurrentThread());
+	SynchronizationContext.SetSynchronizationContext(context);
+	_ = new App();
+});
