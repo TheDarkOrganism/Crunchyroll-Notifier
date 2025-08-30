95ae4367099bfdf87af713d86a8bc4b23f3ea0a7
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-08-30 01:46:36 -0700

    Remove unused using directive
    Removed an unnecessary 'using System.IO;' statement from FileModel.cs to clean up the code.

--- WinUIApp/Models/FileModel.cs
+++ WinUIApp/Models/FileModel.cs
@@ -1,14 +1,11 @@
-﻿
-using System.IO;
-
-namespace WinUIApp.Models
+﻿namespace WinUIApp.Models
 {
 	internal sealed partial class FileModel<TModel, TIModel> : IFileModel<TIModel>
 		where TModel : notnull, ModelBase, TIModel
 		where TIModel : class, IModelBase
 	{
 		private readonly SemaphoreSlim _lock;
-		
+
 		public string File { get; }
 
 		public string ConfigurationSection { get; }
