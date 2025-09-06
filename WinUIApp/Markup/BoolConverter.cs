db88e47cfbfb344eec17d8ced0a28b273edc53d9
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-09-06 14:39:23 -0700

    Add BoolConverter for value conversion in WinUI
    Introduces a BoolConverter class implementing IValueConverter to handle conversion between boolean values and other types in WinUI data binding scenarios.

---
+++ WinUIApp/Markup/BoolConverter.cs
@@ -0,0 +1,46 @@
+﻿namespace WinUIApp.Markup
+{
+	internal sealed partial class BoolConverter : IValueConverter
+	{
+		private static readonly CultureInfo _culture = CultureInfo.InvariantCulture;
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			switch (value)
+			{
+				case bool boolValue:
+					return boolValue;
+				case IConvertible convertible:
+					try
+					{
+						return convertible.ToBoolean(_culture);
+					}
+					catch
+					{
+						return false;
+					}
+				default:
+					return false;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			if (value is bool boolValue)
+			{
+				try
+				{
+					return System.Convert.ChangeType(boolValue, targetType, _culture);
+				}
+				catch
+				{
+					return value;
+				}
+			}
+			else
+			{
+				return value;
+			}
+		}
+	}
+}
