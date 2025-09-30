80bda64f762cfc95801fe3fa66a8ac474c7d6f47
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2025-09-29 18:25:45 -0700

    Enhance TimeSpanConverter to support numeric input
    TimeSpanConverter now handles JSON number tokens by converting them to TimeSpan using seconds. This improves deserialization flexibility for TimeSpan values represented as either strings or numbers.

--- WinUIApp/Converters/TimeSpanConverter.cs
+++ WinUIApp/Converters/TimeSpanConverter.cs
@@ -4,7 +4,9 @@
 	{
 		public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 		{
-			return TimeSpan.TryParse(reader.GetString(), out TimeSpan result) ? result : TimeSpan.Zero;
+			return reader.TokenType == JsonTokenType.String && TimeSpan.TryParse(reader.GetString(), out TimeSpan result)
+				? result
+				: reader.TokenType == JsonTokenType.Number ? TimeSpan.FromSeconds(reader.GetDouble()) : TimeSpan.Zero;
 		}
 
 		public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
